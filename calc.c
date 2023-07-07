93d58e5128df854427124bcd9b0a2a4f7b2015e9
DestinedCodes <yiradesat@gmail.com>
2023-07-07 17:13:27 +0100

    feat: build simple calculator in c (fixed betty)

--- calc.c
+++ calc.c
@@ -1,9 +1,14 @@
 #include "calc.h"
 
+/**
+* main - A program that performs simple arithmetic operations
+* Return: 0 (Success), 1 (Failure)
+*/
+
 int main(void)
 {
 	double num1, num2, result;
-    	char operator;
+	char operator;
 
 	printf("Enter a simple arithmetic expression. Example: 23 * 5\n");
 	printf("(allowed operators: +, -, *, or /)\n");
@@ -11,31 +16,32 @@ int main(void)
 
 	scanf("%lf %c %lf", &num1, &operator, &num2);
 
-	switch (operator) {
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-            		result = num1 - num2;
-            		break;
-        	case '*':
-            		result = num1 * num2;
-           	 	break;
-        	case '/':
-            		if (num2 != 0)
-				result = num1 / num2;
-            		else
-            		{
-                		printf("Error: Division by zero is not allowed.\n");
-				return 1;
-			}
-            		break;
-        	default:
-            		printf("Error: Invalid operator '%c'.\n", operator);
-            		return 1;
+	switch (operator)
+	{
+	case '+':
+		result = num1 + num2;
+		break;
+	case '-':
+		result = num1 - num2;
+		break;
+	case '*':
+		result = num1 * num2;
+		break;
+	case '/':
+		if (num2 != 0)
+			result = num1 / num2;
+		else
+		{
+			printf("Error: Division by zero is not allowed.\n");
+			return (1);
+		}
+		break;
+	default:
+		printf("Error: Invalid operator '%c'.\n", operator);
+		return (1);
 	}
 
 	printf("Result: %.2lf\n", result);
 
-	return 0;
+	return (0);
 }
