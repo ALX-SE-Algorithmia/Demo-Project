9f8f88dbd64802162bdaca519aac47e1dba10e21
ChykB <malikchika86@gmail.com>
2023-07-11 15:54:15 +0100

    Calculator in C language

--- cal.c
+++ cal.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define cal 4
 
-/*Command line calculator using function pointer that prompt the user to select an operation and enter two number to execute the operation.*/
+/*Command line calculator using function pointer that prompt the user to select an operation and enter two number to execute the operation on.*/
 
 /*Declaration of the functions*/
 float sum(float n1, float n2) {return (n1 + n2);}
@@ -11,7 +11,7 @@ float sub(float n1, float n2) {return (n1 - n2);}
 
  int main()
 {
-        /*Pointer to an array of function pointers. The array will contain pointers to the four functions that were declared earlier*/
+        /*Pointer to an array of function pointers. The array contain pointers to the four functions that were declared earlier*/
 	float (*ptr_function[cal]) (float, float) = {sum, multi, div, sub};
         int select;
         float n1, n2;
