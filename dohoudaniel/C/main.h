33011eda833f4207e73392d4fa5e2d3c43ab0185
dohoudaniel <dohoudanielfavour@gmail.com>
2023-07-12 19:26:58 +0100

    Add function prototype definition to main.h

---
+++ dohoudaniel/C/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+double add(int num1, int num2);
+double subtract(int num1, int num2);
+double divide(int num1, int num2);
+double mult(int num1, int num2);
+bool isValidOperator(char operator);
+double print(double result);
+double performCalculation(double num1, double num2, char operator);
+
+#endif
