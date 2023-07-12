a74bd185b2167b8027a3eb0a8602fc2668506c5a
dohoudaniel <dohoudanielfavour@gmail.com>
2023-07-12 22:18:39 +0100

    Update main.h

--- dohoudaniel/C/main.h
+++ dohoudaniel/C/main.h
@@ -5,7 +5,7 @@ double add(int num1, int num2);
 double subtract(int num1, int num2);
 double divide(int num1, int num2);
 double mult(int num1, int num2);
-bool isValidOperator(char operator);
+int isValidOperator(char operator);
 double print(double result);
 double performCalculation(double num1, double num2, char operator);
 void storeUsername(char *username, int maxLength);
