19e432f0e8db96fac58cd79b7c756b82edeb9cff
dohoudaniel <dohoudanielfavour@gmail.com>
2023-07-12 21:59:39 +0100

    A function that prints an exit message.

---
+++ dohoudaniel/C/exit_message.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>    /* To handle string operations */
+#include <stdio.h>    /* Ti handle standard input and output */
+#include <unistd.h>  /* For the sleep function */
+#include <stdlib.h>  /* To include standard library functions */
+
+/**
+ * exitMessage - Prints exit message.
+ *
+ * Return: Exit message.
+ */
+void exitMessage(void)
+{
+	/* Prints this message when the calculation is done. */
+	printf("\n\nThank you for this basic calculator.\n");
+	sleep(2);
+	printf("Recompile to use again...\n");
+	sleep(1.75);
+	printf("Goodbye, %s!\n", username);
+	sleep(2);
+}
