1cabbb8fc503eb874d91d73c45748ed4626a6b06
shazaaly <shaza.aly@gmail.com>
2023-07-26 00:23:17 +0300

    function pointer in calc app

---
+++ shazaAly/calc.h
@@ -0,0 +1,16 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+double add(double a, double b);
+double sub(double a, double b);
+double mul(double a, double b);
+double div(double a, double b);
+double expo(double a, double b);
+
+
+#endif
