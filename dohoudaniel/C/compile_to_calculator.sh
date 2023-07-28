adacfe60b38ad0ecdca1d8e3feff574ac15dfc19
dohoudaniel <dohoudanielfavour@gmail.com>
2023-07-28 21:47:59 +0100

    Update Bash script

--- dohoudaniel/C/compile_to_calculator.sh
+++ dohoudaniel/C/compile_to_calculator.sh
@@ -2,4 +2,4 @@
 
 # This file compiles all the necessary C files into a calculator.
 
-gcc main.c math_functions.c perform_calc.c print_result.c print_message.c store_username.c validate_operator.c exit_message.c -o dohou_calculator
+gcc main.c math_functions.c perform_calc.c print_result.c print_message.c store_username.c validate_operator.c exit_message.c -o daniel_dohou_calculator ; ./daniel_dohou_calculator
