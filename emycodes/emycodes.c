e3974f4d80c518b875d1ba2e9599983cd33e059c
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-29 21:45:44 +0100

    feat: writing main() funciton

---
+++ emycodes/emycodes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    // Introduction and Input Gathering
+    printf("Simple Calculator\n");
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
