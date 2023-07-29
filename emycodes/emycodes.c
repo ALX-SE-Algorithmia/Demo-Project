e896473a726fc83f1aaf2f23c268e3d8e1788eb0
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-29 22:20:29 +0100

    style: indented

--- emycodes/emycodes.c
+++ emycodes/emycodes.c
@@ -1,54 +1,55 @@
 #include <stdio.h>
 
-int main(void) {
-    char operator;
-    double num1, num2, result;
+int main(void)
+{
+char operator;
+double num1, num2, result;
 
-    // Introduction and Input Gathering
-    printf("Simple Calculator\n");
-    printf("Enter first number: ");
-    scanf("%lf", &num1);
+// Introduction and Input Gathering
+printf("Simple Calculator\n");
+printf("Enter first number: ");
+scanf("%lf", &num1);
 
-    // Operator Input Validation Loop
-    while (1) {
-        printf("Enter an operator (+, -, *, /): ");
-        scanf(" %c", &operator);
+// Operator Input Validation Loop
+while (1) {
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
 
-        // Check for valid operator input
-        if (operator == '+' || operator == '-' || operator == '*' || operator == '/')
-            break;
-        else
-            printf("Error: Invalid operator. Please try again.\n");
-    }
+    // Check for valid operator input
+    if (operator == '+' || operator == '-' || operator == '*' || operator == '/')
+        break;
+    else
+        printf("Error: Invalid operator. Please try again.\n");
+}
 
 
-    printf("Enter second number: ");
-    scanf("%lf", &num2);
+printf("Enter second number: ");
+scanf("%lf", &num2);
 
-    // Perform the selected operation
-    switch (operator) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            // Division by zero check
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else {
-                printf("Error: Division by zero is not allowed.\n");
-                return 1; // Exit the program with an error code
+// Perform the selected operation
+switch (operator) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        // Division by zero check
+        if (num2 != 0) {
+        result = num1 / num2;
+        } else {
+            printf("Error: Division by zero is not allowed.\n");
+            return 1; // Exit the program with an error code
             }
-            break;
-    }
+        break;
+}
 
-    // Output the result
-    printf("Result: %lf\n", result);
+// Output the result
+printf("Result: %lf\n", result);
 
-    return (0);
+return (0);
 
