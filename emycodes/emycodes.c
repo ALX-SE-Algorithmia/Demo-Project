205c0491fc34d5c3044d23b640bde516c07bc1a7
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-29 23:32:02 +0100

    style: closed main() function with close-curly braces

--- emycodes/emycodes.c
+++ emycodes/emycodes.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/* Simple calculator program in C for 
+/*
+ * Simple calculator program in C for
  * basic arithmetic operations on two real numbers.
  * The user can input the first number,
  * select an operator (+, -, *, /), and input the second number.
@@ -19,15 +20,16 @@ printf("Enter first number: ");
 scanf("%lf", &num1);
 
 /* Operator Input Validation Loop */
-while (1) {
-    printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &operator);
+while (1)
+{
+printf("Enter an operator (+, -, *, /): ");
+scanf(" %c", &operator);
 
-    /* Check for valid operator input */
-    if (operator == '+' || operator == '-' || operator == '*' || operator == '/')
-        break;
-    else
-        printf("Error: Invalid operator. Please try again.\n");
+/* Check for valid operator input */
+if (operator == '+' || operator == '-' || operator == '*' || operator == '/')
+    break;
+else
+    printf("Error: Invalid operator. Please try again.\n");
 }
 
 
@@ -35,29 +37,30 @@ printf("Enter second number: ");
 scanf("%lf", &num2);
 
 /* Perform the selected operation */
-switch (operator) {
-    case '+':
-        result = num1 + num2;
-        break;
-    case '-':
-        result = num1 - num2;
-        break;
-    case '*':
-        result = num1 * num2;
-        break;
-    case '/':
-        /* Division by zero check*/
-        if (num2 != 0) {
+switch (operator)
+{
+case '+':
+    result = num1 + num2;
+    break;
+case '-':
+    result = num1 - num2;
+    break;
+case '*':
+    result = num1 * num2;
+    break;
+case '/':
+    /* Division by zero check*/
+    if (num2 != 0) {
         result = num1 / num2;
-        } else {
-            printf("Error: Division by zero is not allowed.\n");
-            return 1; /* Exit the program with an error code */
-            }
-        break;
+    } else {
+        printf("Error: Division by zero is not allowed.\n");
+        return 1; /* Exit the program with an error code */
+        }
+    break;
 }
 
 /* Output the result */
 printf("Result: %lf\n", result);
 
 return (0);
-
+}
