913d6aa2feb475a1a6dda73bbbcfc53028bc05ab
Mubarak1A <adesinamubarak123@example.com>
2023-08-06 16:12:23 +0100

    write addition function

---
+++ mubarak/C/add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/*Function - add
+ *takes two args
+ *return an int (addition of its arguments)
+ */
+
+int function add(arg1, arg2){
+    return(arg1 - arg2);
+}
---
+++ mubarak/C/divide.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/*Function - divide
+ *takes two args
+ *return an int (division of its arguments)
+ */
+
+int function divide(arg1, arg2){
+    return (arg1 / arg2);
+}
