382495c7c360f941749056281af742b857743532
Mubarak1A <adesinamubarak123@example.com>
2023-08-07 09:29:29 +0100

    add some interaction

--- mubarak/C/calculator.c
+++ mubarak/C/calculator.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-#include <unistd.h>
+#include "main.h"
+
+/**
+ * Basic Calculator - perform basic arithmetic operation and print answer
+ * handle only four basic operations 
+ * takes in two numbers from the user (standard input)
+ * takes in an arithmetic operator for the four math operations
+ * ‘+’ for Addition, ‘-’ for subtraction, ‘/’ for division, and ‘*’ for multiplication
+ * performs an arithmetic operation based on the arithmetic operator
+ * prints the result to the screen
+ * 
+ * if wrong arithmetic operator other than the one above is received...
+ * or does not receive correct numbers, will print an error to the screen
+ * 
+ * return 0
+ */
 
 int main()
-{
+{   
     printf("Welcome!\n");
     sleep(1);
     printf("Thank you for using Mubarak's Basic Calculator.\n");
@@ -16,49 +31,13 @@ int main()
     printf("- - Subtraction\n");
     printf("* - Multiplication\n");
     printf("/ - Division\n");
-    sleep(2);
+    sleep(1);
 
     double num1, num2;
     char operand;
+    char 
 
-    printf("\nEnter your first number: ");
-    scanf("%lf", &num1);
+    operation(num1, num2, operand);
 
-    printf("Enter your second number: ");
-    scanf("%lf", &num2);
-
-    printf("Enter your operator (eg: +, -, *, or /): ");
-    scanf(" %c", &operand);
-
-    switch (operand)
-    {
-        case '+':
-            printf("The result of %.2lf %c %.2lf = %.2lf\n", num1, operand, num2, (num1 + num2));
-            break;
-        
-        case '-':
-            printf("The result of %.2lf %c %.2lf = %.2lf\n", num1, operand, num2, (num1 - num2));
-            break;
-
-        case '*':
-            printf("The result of %.2lf %c %.2lf = %.2lf\n", num1, operand, num2, (num1 * num2));
-            break;
-
-        case '/':
-            if (num2 != 0) // Check for division by zero
-            {
-                printf("The result of %.2lf %c %.2lf = %.2lf\n", num1, operand, num2, (num1 / num2));
-            }
-            else
-            {
-                printf("Error! Division by zero is not allowed.\n");
-            }
-            break;
-
-        default:
-            printf("Error! Invalid operator. Operator must be +, -, * or /.\n");
-            break;
-    }
-    
     return 0;
 }
