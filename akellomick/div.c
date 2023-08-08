2a9f66ce564836d99064dad0a6efff0dde663830
akellomick <akellomick@gmail.com>
2023-08-08 15:32:13 +0300

    Algorithmia Calculator Demo_project

---
+++ akellomick/div.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * div - divides x by y.
+ * @x: num 1 to compute.
+ * @y: num2 to compute.
+ *
+ * Return: result of x/y
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
