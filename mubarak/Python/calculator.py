2ef7b83d5b0c1958f501218fdf0449edbba538b1
Mubarak1A <adesinamubarak123@example.com>
2023-08-10 01:06:53 +0100

    make the calc interractive

--- mubarak/Python/calculator.py
+++ mubarak/Python/calculator.py
@@ -7,6 +7,19 @@
 
 from operation import operation
 
+print("Welcome!")
+print("Thank you for using Mubarak's Basic Calculator.")
+print("This Calculator takes in two numbers and your arithmetic operator.")
+
+print("\n")
+
+print("The arithmetic operators available are:")
+print("+ - Addition")
+print("- - Subtraction")
+print("* - Multiplication")
+print("/ - Division")
+print("\n")
+
 try:
     operation()
 except ImportError:
